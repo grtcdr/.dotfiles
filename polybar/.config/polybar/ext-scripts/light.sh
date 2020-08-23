fd082cf588db362fd170c32fd3a8ac11f854a520
grtcdr <ba.tahaaziz@gmail.com>
2020-08-23 16:59:46 +0100

    New brightness module using light + visual changes (last commit didn't update)

---
+++ polybar/.config/polybar/ext-scripts/light.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This script requires light, available at: https://github.com/haikarainen/light
+# light requires sudo to function, but you can run it without sudo
+# the easiest method of doing this is to run:
+# sudo chown root:root path/to/light/binary
+# sudo chmod 4775 path/to/light/binary
+
+# Get display brightness value
+sh ~/Scripts/light.sh get
