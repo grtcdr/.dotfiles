95442ef31cab4501f6a6406e717678a59bacbce6
grtcdr <ba.tahaaziz@gmail.com>
2020-10-02 06:01:27 +0100

    Modifications to dotfiles

--- rofi/.config/rofi/scripts/rofiall.sh
+++ rofi/.config/rofi/scripts/rofiall.sh
@@ -11,17 +11,18 @@ option6="Updates"
 option7="Drives"
 option8="Utilities"
 option9="Information"
-
+clipbrd="Clipboard"
 # options passed into variable
-options="$option0\n$option1\n$option2\n$option3\n$option4\n$option5\n$option6\n$option7\n$option8\n$option9"
+options="$option0\n$option1\n$option2\n$option3\n$option4\n$clipbrd\n$option5\n$option6\n$option7\n$option8\n$option9"
 
-chosen="$(echo -e "$options" | rofi -lines 10 -i -dmenu -location 3 -yoffset 34 -p "System")"
+chosen="$(echo -e "$options" | rofi -lines 11 -i -dmenu -location 3 -yoffset 34 -p "System")"
 case $chosen in
     $option0) ropowermenu;;
     $option1) robrightnessmenu;;
     $option2) roxrandrmenu;;
     $option3) roscrotmenu;;
     $option4) roconfiguration;;
+    $clipbrd) clipmenu;;
     $option5) ronetworkmenu;;
     $option6) roupdatemenu;;
     $option7) rodrivemenu;;
